d019f240760179b18db155f971985046684e2133
Adrn <adrianlimsongen@gmail.com>
2018-09-24 17:47:19 +1000

    added utility.c

---
+++ utility.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+** Student name: 	Adrian Lim Song En
+** Student number: 	s3710625
+** Course: 			Advanced Programming Techniques - S2 2018
+******************************************************************************/
+
+#include "utility.h"
+
+void *safeMalloc(size_t)
+{
+  void * p = malloc(size);
+
+  if (p == NULL)
+  {
+    perror(NULL);
+    exit(EXIT_FAILURE);
+  }
+  return p;
+}
